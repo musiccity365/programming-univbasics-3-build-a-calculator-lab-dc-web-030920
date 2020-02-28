6e87a3622b8ddaf7fc3e5901352ce077c9d7219d
Michael David Sackstein <m_sackstein@yahoo.com>
2020-02-28 00:07:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-  num1 + num2
+  addition = num1 + num2
 end
 
 def subtraction(num1, num2)
-  num2 - num1
+  subtraction = num2 - num1
 end
 
 def division(num1, num2)
-  num2 / num1
+  division = num2 / num1
 end
 
 def multiplication(num1, num2)
-  num1 * num2
+  multiplication = num1 * num2
 end
 
 def modulo(num1, num2)
-  num2 % num1
+  modulo = num2 % num1
 end
 
 def square_root(num)
-  Math.sqrt(num)
+  square_root = Math.sqrt(num)
 end
